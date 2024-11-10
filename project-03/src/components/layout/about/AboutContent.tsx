876bf08e6c4cc775cba8bcdca8431f83e19fa09a
Predrag Pavlovikj <predragnbt@gmail.com>
2024-11-10 23:39:43 +0100

    - Finished about SuccesStories component

--- project-03/src/components/layout/about/AboutContent.tsx
+++ project-03/src/components/layout/about/AboutContent.tsx
@@ -10,14 +10,14 @@ export const AboutContent = ({ aboutContent }: Props) => {
       {aboutContent.map((content, index) => (
         <div
           key={index}
-          className="bg-[#323232] text-white px-8 py-10 md:basis-[calc(50%-16px)] flex-1"
+          className="bg-[#323232] text-white px-8 py-10 basis-[calc(50%-16px)] flex-1"
         >
           <div>
             <img src={content.img} alt={content.title} />
           </div>
           <p className="text-[48px] font-[700] my-4">
-            <span className="text-[#FF6F0F]">{content.ourStory}</span>
-            {" "}{content.title}
+            <span className="text-[#FF6F0F]">{content.ourStory}</span>{" "}
+            {content.title}
           </p>
 
           <p>{content.description}</p>
--- project-03/src/components/layout/about/AboutSuccsessStories.tsx
+++ project-03/src/components/layout/about/AboutSuccsessStories.tsx
@@ -7,17 +7,37 @@ interface Props {
 export const AboutSuccsessStories = ({ successStories }: Props) => {
   return (
     <div className="px-[120px] py-[120px]">
-      <p className="text-center font-[700] text-[48px]">
+      <p className="text-center font-[700] text-[48px] mb-16">
         {successStories.title}
       </p>
 
-      <div>
+      <div className="flex overflow-x-scroll gap-6 scrollbar-custom">
         {successStories.data.map((story, index) => (
-          <div key={index}>
-            <div>
-              <img src={story.img} alt="" />
+          <div
+            key={index}
+            className="flex flex-shrink-0 basis-[calc(33%-16px)]"
+          >
+            <div className="flex-shrink-0 mr-4">
+              <img src={story.img} alt={story.challengeTitle} />
             </div>
-            <div></div>
+            <div className="flex flex-col gap-5 border-l-2 border-[#FF6F0F] pl-6 pb-6 mb-16">
+              <p>
+                <span className="font-bold">{story.companyTitle}:</span>{" "}
+                {story.company}
+              </p>
+              <p>
+                <span className="font-bold">{story.challengeTitle}:</span>{" "}
+                {story.challenge}
+              </p>
+              <p>
+                <span className="font-bold">{story.solutionTitle}:</span>{" "}
+                {story.solution}
+              </p>
+              <p>
+                <span className="font-bold">{story.outcomeTitle}:</span>{" "}
+                {story.outcome}
+              </p>
+            </div>
           </div>
         ))}
       </div>
